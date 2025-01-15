3cebe56eea2a7136c5147104a7bfecf8a1ad0300
Alessandro Jodoin <a.jodoin@studenti.unina.it>
2025-01-15 16:12:13 +0100

    prova

---
+++ src/main/java/it/unina/rest_api_dietiestates25/HelloApplication.java
@@ -0,0 +1,9 @@
+package it.unina.rest_api_dietiestates25;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/it/unina/rest_api_dietiestates25/HelloResource.java
@@ -0,0 +1,14 @@
+package it.unina.rest_api_dietiestates25;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
