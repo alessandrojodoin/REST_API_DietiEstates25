c356537025a03b99412286e9273bf873def81d75
Alessandro <a.jodoin@studenti.unina.it>
2026-03-05 16:08:05 +0100

    Admin scripts

--- pushContainerImage.cmd
+++ pushContainerImage.cmd
@@ -1,3 +1,4 @@
+az acr login --name DietiEstatesRESTAPI
 docker image build -t rest_dietiestates25 .
 docker tag rest_dietiestates25 dietiestatesrestapi.azurecr.io/rest_dietiestates25
 docker push dietiestatesrestapi.azurecr.io/rest_dietiestates25
