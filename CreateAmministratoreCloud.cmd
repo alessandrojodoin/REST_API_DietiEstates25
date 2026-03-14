500b6cdb35d024b7f3b4945c8698205b930407ae
Alessandro <a.jodoin@studenti.unina.it>
2026-03-14 13:05:36 +0100

    Bug fixes

--- CreateAmministratoreCloud.cmd
+++ CreateAmministratoreCloud.cmd
@@ -1,3 +1,3 @@
 curl -X POST https://rest-dietiestates25.grayflower-35da6a14.uaenorth.azurecontainerapps.io/api/1.0/auth/amministratore ^
  -H "Content-Type: application/json" ^
- -d "{\"username\":\"Amministratore\",\"agenziaImmobiliare\":\"MyAgenzia\"}"
+ -d "{\"username\":\"Amministratore\",\"agenziaImmobiliare\":\"MyAgenzia\", \"email\":\"default@email.com\"}"
